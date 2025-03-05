27c71468c2cae71d6d81d060406189d6e31d0c69
Chandu1722 <chendichandu.com@gmail.com>
2025-03-05 20:18:39 +0530

    2025-03-05 insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+   int size,i,j,temp,list[100];
+   printf("enter the size of array:");
+   scanf("%d",&size);
+   printf("enter the elements into list:");
+   for(i=0;i<size;i++)
+   {
+       scanf("%d",&list[i]);
+   }
+   for(i=0;i<size;i++)
+   {
+       temp=list[i];
+       j=i-1;
+       while(temp<list[j]&&(j>=0))
+       {
+           list[j+1]=list[j];
+           j=j-1;
+       }
+       list[j+1]=temp;
+   }
+   printf("list after sorting:");
+   for(i=0;i<size;i++)
+    printf("%d ",list[i]);
+  return 0;
+}
