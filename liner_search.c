fa341f3d3a121387faaf713522e812563977e8fb
Chandu1722 <chendichandu.com@gmail.com>
2025-03-06 23:14:32 +0530

    2025-03-06 liner_search.c

---
+++ liner_search.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int linearsearch(int arr[],int size,int key)
+{
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==key)
+            return i;
+    }
+    return -1;
+}
+int main()
+{
+    int arr[]={5,43,34,5,17,22};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int key=43;
+    int index=linearsearch(arr,n,key);
+    if(index!=-1)
+        printf("key found at index %d",index);
+    else
+        printf("key not found");
+  return 0;
+}
